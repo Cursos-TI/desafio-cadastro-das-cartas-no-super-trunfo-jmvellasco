0910753bd3179cc3e4a1028caea289d26b667032
João Marcelo Velasco Rodrigues <jmvr2016@gmail.com>
2025-09-06 01:18:58 +0000

    Dens. Pop. & PIB per

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+    
     // Definição das variáveis para cada atributo das cartas.
 
     char estado1;                                  // Variável do atributo estado da primeira carta
@@ -67,6 +68,16 @@ int main() {
     printf("Digite o número de pontos turísticos da cidade: \n");
     scanf("%d", &pontos_turisticos2);
 
+    // Cálculo da Densidade Populacional:
+
+    float densidade_populacional_1 = populacao1 / area1;
+    float densidade_populacional_2 = populacao2 / area2;
+
+    // Cálculo do PIB per capita:
+
+    double pib_per_capita1 = pib1 / populacao1;
+    double pib_per_capita2 = pib2 / populacao2;
+
     // Exibição dos dados das cartas:
     // Funções printf para exibir os dados cadastrados da primeira carta.
 
@@ -75,9 +86,11 @@ int main() {
     printf("Código: %s\n", codigo1);
     printf("Nome da cidade: %s\n", nome_cidade1);
     printf("População: %d\n", populacao1);
-    printf("Área: %.2f\n", area1);
+    printf("Área: %.2f km²\n", area1);
     printf("PIB: %.0lf\n", pib1);
-    printf("Número de Pontos Turísticos: %d", pontos_turisticos1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f habitantes/km²\n", densidade_populacional_1);
+    printf("PIB per capita: %.0lf", pib_per_capita1);
 
     // Funções printf para exibir os dados cadastrados da segunda carta.
 
@@ -86,9 +99,11 @@ int main() {
     printf("Código: %s\n", codigo2);
     printf("Nome da cidade: %s\n", nome_cidade2);
     printf("População: %d\n", populacao2);
-    printf("Área: %.2f\n", area2);
+    printf("Área: %.2f km²\n", area2);
     printf("PIB: %.0lf\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f habitantes/km²\n", densidade_populacional_2);
+    printf("PIB per capita: %.0lf\n", pib_per_capita2);
 
     return 0;
 
